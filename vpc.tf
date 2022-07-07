5ab0078876a898e9f96c26b087c5b550e03444fe
eduardoreuque <108640756+eduardoreuque@users.noreply.github.com>
2022-07-07 02:21:05 -0400

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA4HY2YYK3X2MU6RND"
+  secret_key = "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"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.project_name}-${var.environment}"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  intra_subnets   = ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
+
+  enable_nat_gateway = true
+  
+  private_subnet_tags = {
+  "kubernetes.io/role/internal-elb" = "1",
+  "kubernetes.io/cluster/${var.project_name}-${var.environment}" = "shared"
+   }
+ 
+  public_subnet_tags = {
+  "kubernetes.io/cluster/${var.project_name}-${var.environment}" = "shared"
+  "kubernetes.io/role/elb" = "1",
+   }
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment
+	project_name = var.project_name
+  }
+}
